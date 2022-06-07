2b9aece302757e0809e67525ffbd3eaea790aae3
Xu <460522390@qq.com>
2022-06-07 20:53:09 +0800

    删除了订单的查询功能

--- xiangmu/src/components/admin-order/admin-order.vue
+++ xiangmu/src/components/admin-order/admin-order.vue
@@ -10,11 +10,6 @@
     <el-card >
       <!--搜索与添加区域-->
       <el-row :gutter="20">
-        <el-col :span="8">
-          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getOrderList">
-            <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
-          </el-input>
-        </el-col>
         <el-col :span="4">
           <el-button type="primary" @click="addDialogVisible = true">添加订单</el-button>
         </el-col>
